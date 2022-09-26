d80d031a625a80402f166b850f656de1678dc76c
Patryk Omiotek <patryk.omiotek@gmail.com>
2022-09-26 13:53:14 +0200

    generator with events

--- src/components/Generator.tsx
+++ src/components/Generator.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { MouseEventHandler, useState } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 
 import { Text } from './Text';
@@ -9,13 +9,15 @@ const Generator = () => {
   // state[0] - var
   // state[1] - callback
 
-  const handleClick = () => {
+  // const handleClick = (e: MouseEvent) => {
+  const handleClick: MouseEventHandler<HTMLHeadingElement | HTMLButtonElement> = (e) => {
+    console.log('event: ', e)
     setUid(uuidv4());
   }
 
   return (
     <div>
-      <h2>Generator</h2>
+      <h2 onClick={handleClick}>Generator</h2>
       <button onClick={handleClick}>Generate</button>
       <Text>{uid}</Text>
     </div>

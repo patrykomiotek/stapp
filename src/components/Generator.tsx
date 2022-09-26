91cdace2c10cebc8cae1fbe99b021848ce8aa1b1
Patryk Omiotek <patryk.omiotek@gmail.com>
2022-09-26 12:32:25 +0200

    generator with desctructurization

--- src/components/Generator.tsx
+++ src/components/Generator.tsx
@@ -4,21 +4,20 @@ import { v4 as uuidv4 } from 'uuid';
 import { Text } from './Text';
 
 const Generator = () => {
-  const state = useState(uuidv4());
+  // const [uid, setUid] = useState(() => uuidv4());
+  const [uid, setUid] = useState(uuidv4());
   // state[0] - var
   // state[1] - callback
 
-  // let uid = uuidv4();
-
   const handleClick = () => {
-    state[1](uuidv4());
+    setUid(uuidv4());
   }
 
   return (
     <div>
       <h2>Generator</h2>
       <button onClick={handleClick}>Generate</button>
-      <Text>{state[0]}</Text>
+      <Text>{uid}</Text>
     </div>
   );
 }

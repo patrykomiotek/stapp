19432124d9deda5f6dd3d8487de00de8a8bab7d2
Patryk Omiotek <patryk.omiotek@gmail.com>
2022-09-26 14:43:23 +0200

    form with info

--- src/components/LoginForm/LoginForm.tsx
+++ src/components/LoginForm/LoginForm.tsx
@@ -12,10 +12,12 @@ const defaultUser: User = {
 }
 
 const LoginForm = () => {
+  const [isSent, setIsSent] = useState(false);
   const [user, setUser] = useState<User>(defaultUser);
 
   const handleSubmit = (event: FormEvent) => {
     event.preventDefault();
+    setIsSent(true);
   }
 
   const handleChange: ChangeEventHandler<HTMLInputElement> = (event) => {
@@ -25,9 +27,24 @@ const LoginForm = () => {
     });
   }
 
+  const showFormInfo = () => {
+    if (isSent) {
+      return <p>Form sent {JSON.stringify(user, null, 2)}</p>
+    }
+  }
+
   return (
     <div>
       <h2>Login form</h2>
+      {/* {isSent ? (<p>Form sent {JSON.stringify(user, null, 2)}</p>) : null} */}
+      {/* {isSent ? (
+        1 === 1 ?
+          <p>Form sent {JSON.stringify(user, null, 2)}</p>
+          : ''
+        )
+      : ''} */}
+      {/* {isSent && <FormInfo />} */}
+      {showFormInfo()}
       <form onSubmit={handleSubmit}>
         <div>
           <label htmlFor="email">E-mail</label>

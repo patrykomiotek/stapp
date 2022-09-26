ea6aceb11d4fd8b14a21db46a35dc93021f97ece
Patryk Omiotek <patryk.omiotek@gmail.com>
2022-09-26 16:24:24 +0200

    fix

--- src/components/LoginForm/LoginForm.tsx
+++ src/components/LoginForm/LoginForm.tsx
@@ -37,7 +37,7 @@ const LoginForm = (props: Props) => {
       user.language = languageFieldRef.current.value;
     }
 
-    // Parent::handleSubmit(user)
+    // Parent::handleSubmit(values, { setSubmiting: true })
   }
 
   return (

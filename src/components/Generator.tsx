40625bd5dba6d769439f88f4e503ca0e69bbba23
Patryk Omiotek <patryk.omiotek@gmail.com>
2022-09-26 12:24:53 +0200

    use state without descruturization

--- src/components/Generator.tsx
+++ src/components/Generator.tsx
@@ -1,11 +1,24 @@
+import { useState } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 
 import { Text } from './Text';
 
 const Generator = () => {
+  const state = useState(uuidv4());
+  // state[0] - var
+  // state[1] - callback
+
+  // let uid = uuidv4();
+
+  const handleClick = () => {
+    state[1](uuidv4());
+  }
+
   return (
     <div>
-      <Text>Test</Text>
+      <h2>Generator</h2>
+      <button onClick={handleClick}>Generate</button>
+      <Text>{state[0]}</Text>
     </div>
   );
 }

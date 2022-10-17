01e63f550647e11314e6334cc6aa620347397e74
Patryk Omiotek <patryk.omiotek@gmail.com>
2022-10-17 16:50:04 +0200

    feat: tests with msw and without

--- src/components/Products/__tests__/Product.spec.tsx
+++ src/components/Products/__tests__/Product.spec.tsx
@@ -1,11 +1,53 @@
 import { render, screen } from "@testing-library/react";
 import { Products } from "../Products";
 
+import { fetchProducts } from '../../../services/products';
+
+const mockedFetchProducts = fetchProducts as jest.Mock;
+
+jest.mock('../../../services/products', () => {
+  return {
+    fetchProducts: jest.fn()
+  }
+});
+
 describe('Product component', () => {
+
+  // with mock service worker
+  // it('should have Prod2', async () => {
+  //   render(<Products />);
+  //   screen.debug();
+
+  //   expect(await screen.findByText('Prod2')).toBeInTheDocument();
+  // });
+
+  // without mock service worker
   it('should have Prod2', async () => {
+    mockedFetchProducts.mockResolvedValue({
+      data: {
+        records: [{
+          id: 1,
+          fields: {
+            name: 'Prod2',
+            price: 123
+          }
+        }]
+      }
+    });
     render(<Products />);
-    // screen.debug();
+    screen.debug();
 
     expect(await screen.findByText('Prod2')).toBeInTheDocument();
+    screen.debug();
+  });
+
+  it('should have error', async () => {
+    mockedFetchProducts.mockRejectedValue({});
+
+    render(<Products />);
+    screen.debug();
+
+    expect(await screen.findByText('Prod2')).toBeInTheDocument();
+    screen.debug();
   });
 });

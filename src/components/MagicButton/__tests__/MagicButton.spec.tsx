8c6b4b34fe180fe1f8dda759099681deb56ad6ba
Patryk Omiotek <patryk.omiotek@gmail.com>
2022-10-17 15:36:12 +0200

    fix: with working test

--- src/components/MagicButton/__tests__/MagicButton.spec.tsx
+++ src/components/MagicButton/__tests__/MagicButton.spec.tsx
@@ -1,10 +1,13 @@
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import { MagicButton } from '../MagicButton';
 
 describe('Magic button component', () => {
   it('should render MagicButton', () => {
     const handler = jest.fn();
     render(<MagicButton handleMouseEnter={handler}>Click me</MagicButton>);
+
+    userEvent.click(screen.getByRole('button'));
 
     expect(handler).toHaveBeenCalled();
   });
